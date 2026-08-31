05a24f8fa56a3af9b733568810d8baeab3ab78d1
skorohodovsa <sergey.skor007@gmain.com>
2026-08-31 21:56:35 +0300

    [0.2.0] Изменены имена преподавателей 5 семестр

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # ---------------------------- Основные настройки ---------------------------- #
 release = "0.0.1"
-version = "0.1.50"
+version = "0.2.0"
 version_site = version
 
 author = "SerKin0"
