5c4194a19b1d911bfe95a7a650666bd666d3c590
skorohodovsa <sergey.skor007@gmain.com>
2026-09-07 17:49:12 +0300

    [0.2.2] Добавлен задачник по ТИиК 5 семестр

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # ---------------------------- Основные настройки ---------------------------- #
 release = "0.0.1"
-version = "0.2.1"
+version = "0.2.2"
 version_site = version
 
 author = "SerKin0"
